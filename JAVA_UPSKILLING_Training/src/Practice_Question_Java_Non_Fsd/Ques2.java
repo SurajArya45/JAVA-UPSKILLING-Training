209db011a6e12b12e0f254e3dbbf3aa262483d90
suarya <suarya@lin80033060.corp.capgemini.com>
2023-04-20 14:05:07 +0530

    Ques2

---
+++ JAVA_UPSKILLING_Training/src/Practice_Question_Java_Non_Fsd/Ques2.java
@@ -0,0 +1,30 @@
+package Practice_Question_Java_Non_Fsd;
+
+public class Ques2 {
+public static void main(String[] args) {
+	int i=100;
+	String s=Integer.toString(i);
+	System.out.println(i);
+	
+	
+	
+	String s1="200";  
+	int j=Integer.parseInt(s1);  
+	System.out.println(j); 
+	
+	
+	String s2="236";
+	double d=Double.parseDouble(s2);
+	System.out.println(d);
+	
+	
+	double DoubleValue = 3.6987;
+	int IntValue = (int) DoubleValue;
+	System. out. println(DoubleValue + " is now " + IntValue);
+	
+	
+	
+	}
+	}
+
+
